dd2a314667296cc38c5862aefe63e45a86491b9d
anurag-50 <anuragpandey2450@gmail.com>
2026-06-08 18:35:24 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    bool canEatAll(vector<int>& piles, int h,int k) {
+        long long hour=0;
+        for(int banana:piles) {
+            hour+=(banana+k-1)/k;
+            if(hour>h) {
+                return false;
+            }
+        }
+        return hour<=h;
+    }
+
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int start=1,end=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(start<=end) {
+            int mid=start+(end-start)/2;
+            if(canEatAll(piles,h,mid)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+};
